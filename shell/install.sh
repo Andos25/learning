ec886eaa4fc7130f0951a9ab57ac9f9071d24d9d
Andos-test <283613697@qq.com>
2013-11-22 01:01:49 +0800

    finish apache install

--- shell/install.sh
+++ shell/install.sh
@@ -13,6 +13,7 @@ cd httpd-2.4.6
 sudo ./configure --prefix=/usr/local/apache2/ --enable-proxy --enable-ssl --enable-cgi --enable-rewrite --enable-so --enable-module=so
 sudo make
 sudo make install
-sudo chmod 770 -R /usr/local/apache2
+sudo chmod 777 -R /usr/local/apache2
 sudo echo "ServerName 0.0.0.0:80" >> /usr/local/apache2/conf/httpd.conf
+sudo echo "LoadModule slotmem_shm_module modules/mod_slotmem_shm.so" >> /usr/local/apache2/conf/httpd.conf
 sudo /usr/local/apache2/bin/apachectl start
