bc477b33f259e9393ca3e31cba32238c47fdbd8c
Andos <andos@andos-VirtualBox.(none)>
2013-11-29 22:51:06 +0800

    finish https

--- shell/install.sh
+++ shell/install.sh
@@ -2,19 +2,34 @@
 
 ROOT_UID=0
 
-#检测是否以超级权限运行
-#if [ "$UID" -ne "$ROOT_UID" ]
-#then
-#    sudo su
-#    tar zxvf httpd-2.4.6.tar.gz
-#fi
-tar zxvf httpd-2.4.6.tar.gz
-cd httpd-2.4.6
-#sudo ./configure --prefix=/usr/local/apache2/ --enable-proxy --enable-ssl --enable-cgi --enable-rewrite --enable-so --enable-module=so
-sudo ./configure --enable-so --enable-so
+tar zxvf httpd-2.2.26.tar.gz
+cd httpd-2.2.26
+sudo ./configure --prefix=/usr/local/apache2/ --enable-proxy --enable-ssl --enable-cgi --enable-rewrite --enable-so --enable-module=so
 sudo make
 sudo make install
 sudo chmod 777 -R /usr/local/apache2
 sudo echo "ServerName 0.0.0.0:80" >> /usr/local/apache2/conf/httpd.conf
-sudo echo "LoadModule slotmem_shm_module modules/mod_slotmem_shm.so" >> /usr/local/apache2/conf/httpd.conf
+sudo echo "AddType application/x-httpd-php .php" >> /usr/local/apache2/conf/httpd.conf
+sudo echo "Include conf/extra/httpd-ssl.conf"
+mkdir /usr/local/apache2/ssl
+sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /usr/local/apache2/ssl/apache.key -out /usr/local/apache2/ssl/apache.crt
+sudo echo "SSLCertificateFile \"/usr/local/apache2/ssl/apache.crt\"">>/usr/local/apache2/conf/extra/httpd-ssl.conf
+sudo echo "SSLCertificateKeyFile \"/usr/local/apache2/ssl/apache.key\"">>/usr/local/apache2/conf/extra/httpd-ssl.conf
 sudo /usr/local/apache2/bin/apachectl start
+sudo apt-get install python-dev
+exit
+cd ../
+tar zxvf libxml2-2.9.1.tar.gz 
+cd libxml2-2.9.1
+sudo ./configure --prefix=/usr/local/libxml2 
+sudo make
+sudo make install
+
+cd ../
+tar jxvf php-5.5.6.tar.bz2
+cd php-5.5.6
+sudo ./configure --prefix=/usr/local/php --with-libxml-dir=/usr/local/libxml2 --with-apxs2=/usr/local/apache2/bin/apxs
+sudo make
+sudo make install
+sudo ln -s /usr/local/php/bin/php /usr/bin/php
+sudo /usr/local/apache2/bin/apachectl restart
