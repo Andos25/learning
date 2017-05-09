9cd9e62ccd41d10a46ee60118ad86aecd2f781b0
Chujie He <chhe@microsoft.com>
2017-05-09 18:04:47 +0800

    add

---
+++ DesignPattern/IOC/IOC/ConsoleOutput.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IOC
+{
+    public class ConsoleOutput : IOutput
+    {
+        public void Write(string content)
+        {
+            Console.WriteLine(content);
+        }
+    }
+}
---
+++ DesignPattern/IOC/IOC/YesterdayWriter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IOC
+{
+    class YesterdayWriter: IDateWriter
+    {
+        private IOutput _output;
+        public YesterdayWriter(IOutput output)
+        {
+            this._output = output;
+        }
+
+        public void WriteDate()
+        {
+            this._output.Write(DateTime.Today.AddDays(-1).ToString());
+        }
+    }
+}
